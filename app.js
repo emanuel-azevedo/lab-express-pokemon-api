d29748c3c68660c252126a75b645bc5aac49ecc1
Emanuel Azevedo <emanuelcansancao@hotmail.com>
2022-07-12 22:47:29 -0300

    Iterações 1,2 e 3 resolvidas.

--- app.js
+++ app.js
@@ -8,5 +8,37 @@ const allPokemon = require("./data");
 const app = express();
 
 // -- Define your route listeners here! --
+app.get("/pokemon", (req, res) => {
+    res.send(allPokemon)
+})
+
+app.get("/pokemon/:id", (req, res) => {
+    const id = req.params.id
+    const pokemon = allPokemon.find( elemento => {
+        return elemento.id === parseInt(id, 10)
+    })
+    res.send(pokemon)
+})
+
+app.get("/search", (req, res) => {
+    const name = req.query.name
+    const type = req.query.type
+    
+    const pokemon = allPokemon.find( elemento => {
+        return elemento.name === name
+    })
+
+    let allPkm = []
+    allPokemon.map( poke => {
+        if (poke.types.includes(type)) {
+            allPkm.push(poke)            
+            return allPkm
+        }
+    })
+    
+    res.send( name ? pokemon : allPkm)
+})
+
+
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
